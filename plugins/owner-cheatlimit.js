7dfa9a796e6d825bf4128b762d3b1cec340e569e
SaepEp <105292856+SaepEp@users.noreply.github.com>
2022-11-07 15:02:07 +0700

    Update owner-cheatlimit.js

--- plugins/owner-cheatlimit.js
+++ plugins/owner-cheatlimit.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender]
         conn.reply(m.chat, `*Succes Cheat !*`, m)
-        global.db.data.users[m.sender].money = 10000000
-        global.db.data.users[m.sender].limit = 10000000
-        global.db.data.users[m.sender].level = 1000
+        global.db.data.users[m.sender].money = 100000000000
+        global.db.data.users[m.sender].limit = 100000000000
+        global.db.data.users[m.sender].level = 10000
         global.db.data.users[m.sender].exp = 1000000000
 }
 handler.command = /^(curang)$/i
