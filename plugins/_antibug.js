a9cb55d217f2c93a684ddc5461838372ea1317ec
SaepEp <105292856+SaepEp@users.noreply.github.com>
2022-11-08 11:03:56 +0700

    Update _antibug.js

--- plugins/_antibug.js
+++ plugins/_antibug.js
@@ -2,8 +2,8 @@ let handler = m => m
 
 handler.all = async function (m) {
     let user = global.db.data.users[m.sender]
-    if ((user.money * 1) > 99999998) {
-        user.money = 99999999
+    if ((user.money * 1) > 99999998999999999999999999) {
+        user.money = 9999999999999999999999999999999
     } else if ((user.money * 1) < 0) {
         user.money = 0
     }
